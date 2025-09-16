ff25ce30db3be923e1f6c1035e1b04a322eca594
ArkhamCookie <git@mail.theuntitledproject.dev>
2025-09-16 15:42:31 -0500

    feat: change uri to match repo

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use cxx_qt_build::{CxxQtBuilder, QmlModule};
 fn main() {
 	CxxQtBuilder::new()
 		.qml_module(QmlModule {
-			uri: "org.ac.tutorial",
+			uri: "org.ac.starter",
 			qml_files: &["src/qml/Main.qml"],
 			rust_files: &["src/main.rs"],
 			..Default::default()
